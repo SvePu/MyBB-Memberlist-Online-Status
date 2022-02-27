ef3f50993021f7049aafa9ad0eee534dc1e21702
Sven <SvePu@users.noreply.github.com>
2022-02-27 19:56:59 +0100

    Update memberlist_onlinestatus.php

--- Upload/inc/plugins/memberlist_onlinestatus.php
+++ Upload/inc/plugins/memberlist_onlinestatus.php
@@ -1,24 +1,29 @@
 <?php
 
 // Disallow direct access to this file for security reasons
-if(!defined('IN_MYBB'))
+if (!defined('IN_MYBB'))
 {
     die('Direct initialization of this file is not allowed.');
 }
 
-if(defined('THIS_SCRIPT'))
+if (defined('THIS_SCRIPT'))
 {
     global $templatelist;
 
-    if(isset($templatelist))
+    if (isset($templatelist))
     {
         $templatelist .= ',';
     }
 
-    if(THIS_SCRIPT== 'memberlist.php')
+    if (THIS_SCRIPT == 'memberlist.php')
     {
-        $templatelist .= 'postbit_online, postbit_away, postbit_offline';
+        $templatelist .= 'memberlist_user_onlinestatus, memberlist_user_online, memberlist_user_away, memberlist_user_offline';
     }
+}
+
+if (!defined('IN_ADMINCP'))
+{
+    $plugins->add_hook('memberlist_user', 'memberlist_onlinestatus_run');
 }
 
 function memberlist_onlinestatus_info()
@@ -37,35 +42,62 @@ function memberlist_onlinestatus_info()
 
 function memberlist_onlinestatus_activate()
 {
-    require_once MYBB_ROOT.'inc/adminfunctions_templates.php';
-    find_replace_templatesets('memberlist_user', '#'.preg_quote('{$user[\'profilelink\']}').'#', "{\$user['profilelink']}{\$user['onlinestatus']}");
+    global $db, $mybb;
+
+    $templatearray = array(
+        'memberlist_user_onlinestatus' => '<div style="margin-top: 0.5em; font-weight: bold;">{$user[\'onlinestatus\']}</div>',
+        'memberlist_user_away' => '<a href="{$user[\'profilelink_plain\']}"><span style="color: tan;">{$lang->postbit_status_away}</span></a>',
+        'memberlist_user_offline' => '<span class="offline">{$lang->postbit_status_offline}</span>',
+        'memberlist_user_online' => '<a href="online.php"><span class="online">{$lang->postbit_status_online}</span></a>'
+    );
+
+    foreach ($templatearray as $name => $template)
+    {
+        $template = array(
+            'title' => $db->escape_string($name),
+            'template' => $db->escape_string($template),
+            'version' => $mybb->version_code,
+            'sid' => -2,
+            'dateline' => TIME_NOW
+        );
+
+        $db->insert_query('templates', $template);
+    }
+
+    require_once MYBB_ROOT . 'inc/adminfunctions_templates.php';
+    find_replace_templatesets('memberlist_user', '#' . preg_quote('{$user[\'userstars\']}') . '#', "{\$user['userstars']}\n{\$user['onlinestatus_container']}");
 }
 
 function memberlist_onlinestatus_deactivate()
 {
-    require MYBB_ROOT.'/inc/adminfunctions_templates.php';
-    find_replace_templatesets('memberlist_user', '#'.preg_quote('{$user[\'onlinestatus\']}').'#', '');
+    global $db;
+    $db->delete_query('templates', "title IN ('memberlist_user_onlinestatus', 'memberlist_user_away', 'memberlist_user_offline', 'memberlist_user_online')");
+
+    require MYBB_ROOT . '/inc/adminfunctions_templates.php';
+    find_replace_templatesets('memberlist_user', '#' . preg_quote("\n{\$user['onlinestatus_container']}") . '#', '');
 }
 
 function memberlist_onlinestatus_run(&$user)
 {
-    global $mybb, $theme, $templates, $lang;
+    global $mybb, $templates, $lang;
     $lang->load('global');
     $timecut = TIME_NOW - $mybb->settings['wolcutoff'];
-    if($user['lastactive'] > $timecut && ($user['invisible'] != 1 || $mybb->usergroup['canviewwolinvis'] == 1) && $user['lastvisit'] != $user['lastactive'])
+    if ($user['lastactive'] > $timecut && ($user['invisible'] != 1 || $mybb->usergroup['canviewwolinvis'] == 1) && $user['lastvisit'] != $user['lastactive'])
     {
-        eval("\$user['onlinestatus'] = \"".$templates->get("postbit_online")."\";");
+        eval("\$user['onlinestatus'] = \"" . $templates->get("memberlist_user_online") . "\";");
     }
     else
     {
-        if($user['away'] == 1 && $mybb->settings['allowaway'] != 0)
+        if ($user['away'] == 1 && $mybb->settings['allowaway'] != 0)
         {
-            eval("\$user['onlinestatus'] = \"".$templates->get("postbit_away")."\";");
+            $user['profilelink_plain'] = get_profile_link($user['uid']);
+            eval("\$user['onlinestatus'] = \"" . $templates->get("memberlist_user_away") . "\";");
         }
         else
         {
-            eval("\$user['onlinestatus'] = \"".$templates->get("postbit_offline")."\";");
+            eval("\$user['onlinestatus'] = \"" . $templates->get("memberlist_user_offline") . "\";");
         }
     }
+
+    eval("\$user['onlinestatus_container'] = \"" . $templates->get("memberlist_user_onlinestatus") . "\";");
 }
-$plugins->add_hook('memberlist_user', 'memberlist_onlinestatus_run');
